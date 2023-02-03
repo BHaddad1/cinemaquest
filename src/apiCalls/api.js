79ac8005f4ee68976c7a8f340cae81a987905763
Blanche Haddad <blancherachel@gmail.com>
2023-02-03 16:59:43 -0700

    feat: Add API calls and file

---
+++ src/apiCalls/api.js
@@ -0,0 +1,12 @@
+const getData = (endpoint) => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${endpoint}`)
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+        } else {
+          throw new Error("Sorry! Failed to load, please try again later.");
+        }
+      })
+}
+
+export default getData;
