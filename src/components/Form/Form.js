51a2749a886f16ba8cbaf377d0666261aaa2259a
Blanche Haddad <blancherachel@gmail.com>
2023-02-03 15:27:18 -0700

    fix: Save styling

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,41 +1,43 @@
-import React, {Component} from "react";
-import './Form.css';
-import icon from '../../images/popcorn.png'
+import React, { Component } from "react";
+import "./Form.css";
+import icon from "../../images/popcorn.png";
 
 class Form extends Component {
-    constructor() {
-        super();
-        this.state = {
-					title: ''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+    };
+  }
 
-		handleChange = (event) => {
-			const { name, value } = event.target;
-			this.setState({[name]: value})
-		}
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
 
-		handleClick = () => {
-			console.log("Return and fix");
-		}
+  handleClick = () => {
+    console.log("Return and fix");
+  };
 
-    render() {
-			return (
-				<header className="nav">
-					<img className="logo" src={icon} alt="Happy popcorn bucket"/> 
-					<h1 className="title">CinemaQuest</h1>
-					<input 
-						type='text'
-						placeholder="Movie Title"
-						value={this.state.title}
-						name="title"
-						onChange={(event) => this.handleChange(event)}
-						className="search-field"
-					/>
-					<button className="button" onClick={this.handleClick}>Search</button>
-				</header>    
-        )
-			}
-		}
+  render() {
+    return (
+      <header className="nav">
+        <img className="logo" src={icon} alt="Happy popcorn bucket" />
+        <h1 className="title">CinemaQuest</h1>
+        <input
+          type="text"
+          placeholder="Movie Title"
+          value={this.state.title}
+          name="title"
+          onChange={(event) => this.handleChange(event)}
+          className="search-field"
+        />
+        <button className="button" onClick={this.handleClick}>
+          Search
+        </button>
+      </header>
+    );
+  }
+}
 
 export default Form;
