8341348cc27291486443240606b3c7fdf03aa387
Blanche Haddad <blancherachel@gmail.com>
2023-02-03 16:59:29 -0700

    feat: Display single movie information, update props

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -1,27 +1,32 @@
 import React from "react";
 import './SingleMovie.css'; 
 
-const SingleMovie = ({ image, title, releaseDate, overview, rating, genre, budget, revenue, runtime, tagline, handleBackButton }) => {
+const SingleMovie = ({ singleMovie, handleBackButton }) => {
   
+	let total = singleMovie.runtime / 60
+	let time = total.toString();
+	let minutes = time.substring(2, 4);
+
 	return (
 		<section className="single-movie">
 			<button onClick={handleBackButton}>Go Back Home</button>
 			<div className="poster">
-				<img src={image} alt="movie poster"/> 
+				<img src={singleMovie["poster_path"]} alt="Movie Poster"/> 
 			</div>
 			<div className="movie-info">
-				<h2>{title}</h2>
-				<h3>{releaseDate}</h3>
-				<h3>{tagline}</h3>
-				<h3>{genre}</h3>
-				<h3>{rating}</h3>
-				<p>{overview}</p>
-				<p>{runtime}</p>
-				<p>{budget}</p>
-				<p>{revenue}</p>
+				<h2>{singleMovie.title}</h2>
+				<h3>{singleMovie.tagline}</h3>
+				<h3>Released:  {singleMovie["release_date"].replaceAll("-", "/")}</h3>
+				<h3>Average Rating:  {singleMovie["average_rating"]}</h3>
+				<h3>Genres:  {singleMovie.genres.join(", ")}</h3>
+				<p>Overview:  {singleMovie.overview}</p>
+				<p>Budget:  ${singleMovie.budget}</p>
+				<p>Total Revenue:  ${singleMovie.revenue}</p>
+				<p>Total Runtime:  <span>{time[0]} hours</span>  <span>{minutes} minutes</span></p>
 			</div>
 		</section>
 	)
 }
 
 export default SingleMovie;
+
