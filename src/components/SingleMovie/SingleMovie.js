ada889eb95e23beff91552628707e0d2855af898
Andy Cerveny <andrewf0628@gmail.com>
2023-02-06 19:54:29 -0500

    altered the router created a new object that has all of the logic and updated singleMovie render

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -15,7 +15,34 @@ class SingleMovie extends Component {
 	componentDidMount() {
 		getData(`/movies/${this.props.movieID}`)
 			.then(data => {
-				this.setState({singleMovie: data.movie, isLoading: false})
+				const title = data.movie.title;
+				const tagline = data.movie.tagline;
+				const releaseDate = data.movie["release_date"].replaceAll("-", "/");
+				const averageRating = data.movie["average_rating"];
+				const genres = data.movie.genres.join(", ");
+				const overview = data.movie.overview;
+				const budget = data.movie.budget.toLocaleString("en-US");
+				const revenue = data.movie.revenue.toLocaleString("en-US");
+				const runtime = data.movie.runtime;
+				const backDropImg = data.movie['backdrop_path'];
+				const posterPath = data.movie['poster_path']; 
+				
+				const alteredSingleMovie = {
+					title: title,
+					tagline: tagline,
+					release_Date: releaseDate,
+					average_rating:averageRating,
+					genres:genres,
+					overview: overview,
+					budget:budget,
+					revenue: revenue,
+					runtime: runtime,
+					backdrop_path: backDropImg,
+					poster_path: posterPath,
+				}
+				
+
+				this.setState({singleMovie: alteredSingleMovie, isLoading: false})
 			})
 	}
   
@@ -36,22 +63,22 @@ class SingleMovie extends Component {
 				<section className="single-movie">
 					<div className="poster-container">
 						<img 
-							// src={this.state.singleMovie["poster_path"]} 
+							src={this.state.singleMovie["poster_path"]} 
 							alt="Movie Poster" 
 							className="poster"
 						/> 
-					</div>
-					<div className="movie-info">
+					</div> 
+					 <div className="movie-info">
 						<h2 className="movie-title">{this.state.singleMovie.title}</h2>
 						<h3>Tagline: <span className="info">{this.state.singleMovie.tagline}</span> </h3>
-						<h3>Released: <span className="info">{this.state.singleMovie["release_date"].replaceAll("-", "/")} </span> </h3>
+						<h3>Released: <span className="info">{this.state.singleMovie["release_date"]} </span> </h3>
 						<h3>Average Rating: <span className="info">{this.state.singleMovie["average_rating"]}</span> </h3>
-						<h3>Genres: <span className="info">{this.state.singleMovie.genres.join(", ")} </span> </h3>
+						<h3>Genres: <span className="info">{this.state.singleMovie.genres} </span> </h3>
 						<p>Overview: <span className="info">{this.state.singleMovie.overview} </span> </p>
-						<p>Budget: <span className="info">${this.state.singleMovie.budget.toLocaleString("en-US")} </span> </p>
-						<p>Total Revenue: <span className="info"> ${this.state.singleMovie.revenue.toLocaleString("en-US")} </span> </p>
+						<p>Budget: <span className="info">${this.state.singleMovie.budget} </span> </p>
+						<p>Total Revenue: <span className="info"> ${this.state.singleMovie.revenue} </span> </p>
 						<p>Total Runtime: <span className="info"></span><span>{this.getDisplayTime()}</span></p>
-					</div>
+					</div> 
 				</section>
 			</section>
 		)
@@ -59,4 +86,5 @@ class SingleMovie extends Component {
 }
 
 export default SingleMovie;
+
 
