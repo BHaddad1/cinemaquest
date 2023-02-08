ae6d71b508c1b0f7aeb53491a45402d294a4506e
Blanche Haddad <blancherachel@gmail.com>
2023-02-07 19:47:14 -0700

    fix: Make tests more specific

--- cypress/e2e/allmoviespage.cy.js
+++ cypress/e2e/allmoviespage.cy.js
@@ -8,14 +8,14 @@ describe('CinemaQuest Main Page', () => {
    })
    it("Should have a header form", () => {
     cy.get(".title").contains("CinemaQuest");
-    cy.get(".logo");
-    cy.get(".search-field");
-    cy.get("button");
+    cy.get(".logo").should("be.visible");
+    cy.get(".search-field").should("be.visible");
+    cy.get("button").should("be.visible");
    });
    it("Should display all movies", () => {
-    cy.get(".movies-container");
-    cy.get(".movie-card");
-    cy.get(".movie-image");
+    cy.get(".movies-container").should("be.visible");
+    cy.get(".movie-card").should("be.visible");
+    cy.get(".movie-image").should("be.visible");
    });
    it("Should be able to view movie details once a user clicks on a card", () => {
     cy.get('.movie-card').first().click();
