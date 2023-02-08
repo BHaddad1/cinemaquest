119b2e2ce7adaeb48b0ae34c98c4f7d4c0ed2198
Blanche Haddad <blancherachel@gmail.com>
2023-02-07 19:39:45 -0700

    feat: Add tests for main page

--- cypress/e2e/allmoviespage.cy.js
+++ cypress/e2e/allmoviespage.cy.js
@@ -1,5 +1,23 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('CinemaQuest Main Page', () => {
+  beforeEach(()=> {
+    cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
+      method: "GET",
+      fixture: "../fixtures/allMovies"
+    })
+    cy.visit("http://localhost:3000/")
+   })
+   it("Should have a header form", () => {
+    cy.get(".title").contains("CinemaQuest");
+    cy.get(".logo");
+    cy.get(".search-field");
+    cy.get("button");
+   });
+   it("Should display all movies", () => {
+    cy.get(".movies-container");
+    cy.get(".movie-card");
+    cy.get(".movie-image");
+   });
+   it("Should be able to view movie details once a user clicks on a card", () => {
+    cy.get('.movie-card').first().click();
+   });
 })
