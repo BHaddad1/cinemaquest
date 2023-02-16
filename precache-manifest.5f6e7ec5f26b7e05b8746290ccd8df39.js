5c2f3130ed9a32afd23bdbdfec8ec767eeea9c0a
Blanche Haddad <blancherachel@gmail.com>
2023-02-16 09:51:02 -0700

    Updates

---
+++ precache-manifest.5f6e7ec5f26b7e05b8746290ccd8df39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8168bdcc51aabe18877",
+    "url": "/static/css/main.ae34b137.chunk.css"
+  },
+  {
+    "revision": "d8168bdcc51aabe18877",
+    "url": "/static/js/main.d8168bdc.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "/static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "89e764e659b600c40c97",
+    "url": "/static/js/2.89e764e6.chunk.js"
+  },
+  {
+    "revision": "fb573bd4075a37a7f80b",
+    "url": "/static/js/runtime~main.fb573bd4.js"
+  },
+  {
+    "revision": "77544ce0e8aa131336915f319567c42e",
+    "url": "/static/media/popcorn.77544ce0.png"
+  },
+  {
+    "revision": "08f38ade6c0401ce29254260235cb186",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cinemaquest/precache-manifest.81266a80de554dda9d2ca88a7da3187c.js"
+  "/precache-manifest.5f6e7ec5f26b7e05b8746290ccd8df39.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cinemaquest/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
